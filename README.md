c43a3c1cbcd82f35c83560b0c5780107cbd30e1f
Roman Lysychkin <cxllmerichie@gmail.com>
2023-01-08 15:51:06 +0100

    screen_brightness_control module last version has errors, came back to last tested, works

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## Install
 - download "assets" folder and "build/BrightnessControl.exe" file
 - put them into one directory
-- create a shortcut for convenient usage 
+- create a shortcut for convenient usage
 ## Usage
 - works from tray
 - Ctrl+Q to close the app (from tray)
--- build/build.py
+++ build/build.py
@@ -1,14 +1,16 @@
-import subprocess
+from subprocess import PIPE, Popen
+from os import getcwd, path, pardir
 
 
-name = 'BrightnessControl'
-root = 'C:\\Projects\\Python\\BrightnessControl-Desktop-'
-main = f'{root}\\main.py'
-build = f'{root}\\build'
-icon = f'{root}\\assets\\icon.ico'
+if __name__ == '__main__':
+    name = 'BrightnessControl'
+    root = path.abspath(path.join(getcwd(), pardir))
+    main = f'{root}\\main.py'
+    build = f'{root}\\build'
+    icon = f'{root}\\assets\\icon.ico'
 
-command = f'pyinstaller --onefile --windowed --specpath {build} --workpath {build} --distpath {build} --icon={icon} --name={name} {main}'
+    command = f'pyinstaller --onefile --windowed --specpath {build} --workpath {build} --distpath {build} --icon={icon} --name={name} {main}'
 
-process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-out, err = process.communicate()
-print(f'{command}\n{out.decode()}')
+    process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE)
+    out, err = process.communicate()
+    print(f'{command}\n{out.decode()}')
