2023852528db891f8502a55ce91f8d94266e9a89
Roman Lysychkin <cxllmerichie@gmail.com>
2023-01-19 01:53:54 +0100

    New release, build automation done through python, not using *.sh more.

--- build/build.py
+++ build/build.py
@@ -1,16 +1,29 @@
 from subprocess import PIPE, Popen
 from os import getcwd, path, pardir
-
+from shutil import rmtree as rmdir
+from os import remove as rmfile
+from zipfile import ZipFile
 
 if __name__ == '__main__':
-    name = 'BrightnessControl'
-    root = path.abspath(path.join(getcwd(), pardir))
-    main = f'{root}\\main.py'
-    build = f'{root}\\build'
-    icon = f'{root}\\assets\\icon.ico'
+    VERSION = '2.1'
+    NAME = 'BrightnessControl'
+    ROOT = path.abspath(path.join(getcwd(), pardir))
+    MAIN = f'{ROOT}\\main.py'
+    BUILD = f'{ROOT}\\build'
+    ICON = f'{ROOT}\\assets\\icon.ico'
 
-    command = f'pyinstaller --onefile --windowed --specpath {build} --workpath {build} --distpath {build} --icon={icon} --name={name} {main}'
+    command = f'pyinstaller --onefile --windowed --specpath {BUILD} --workpath {BUILD} --distpath {BUILD} --icon={ICON} --name={NAME} {MAIN}'
 
     process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE)
     out, err = process.communicate()
     print(f'{command}\n{out.decode()}')
+
+    rmdir(f'{BUILD}\\{NAME}')
+    rmfile(f'{BUILD}\\{NAME}.spec')
+
+    release = ZipFile(f'{ROOT}\\Releases\\{NAME}-v{VERSION}.zip', 'w')
+    release.write(f'{NAME}.exe')
+    release.write(f'{ROOT}\\assets')
+    release.close()
+
+    rmfile(f'{BUILD}\\{NAME}.exe')
